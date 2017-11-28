179a771d911dcfb876f204e89f73e4e047eb633e
Mark Bello <mcbello51286@gmail.com>
2017-11-28 15:15:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown
     counter += 1
   end
 end
+
+def countdown_with_sleep
+  counter = 10
+  while counter >= 0
+    puts "#{counter} SECOND(S)"
+    counter += 1
+    sleep 1
+  end
+end
